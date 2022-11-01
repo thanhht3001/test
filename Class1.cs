5d893aaf00c24f2994ada175232957d1e88a82dc
ThanhHT <90333333+thanhht3001@users.noreply.github.com>
2022-11-01 07:03:33 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,74 @@
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace WinFormsApp4
+{
+    internal class Class1
+    {
+        private string filePath = "";
+
+        public void SaveFilePath(string fileName)
+        {
+            filePath = fileName;
+        }
+
+        public void AddDataTofile()
+        {
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+            if (xlApp == null)
+            {
+                MessageBox.Show("Excel is not properly installed!!");
+                return;
+            }
+
+
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            xlWorkSheet.Cells[1, 1] = "";
+            xlWorkSheet.Cells[1, 2] = "Student1";
+            xlWorkSheet.Cells[1, 3] = "Student2";
+            xlWorkSheet.Cells[1, 4] = "Student3";
+
+            xlWorkSheet.Cells[2, 1] = "Term1";
+            xlWorkSheet.Cells[2, 2] = "80";
+            xlWorkSheet.Cells[2, 3] = "65";
+            xlWorkSheet.Cells[2, 4] = "45";
+
+            xlWorkSheet.Cells[3, 1] = "Term2";
+            xlWorkSheet.Cells[3, 2] = "78";
+            xlWorkSheet.Cells[3, 3] = "72";
+            xlWorkSheet.Cells[3, 4] = "60";
+
+            xlWorkSheet.Cells[4, 1] = "Term3";
+            xlWorkSheet.Cells[4, 2] = "82";
+            xlWorkSheet.Cells[4, 3] = "80";
+            xlWorkSheet.Cells[4, 4] = "65";
+
+            xlWorkSheet.Cells[5, 1] = "Term4";
+            xlWorkSheet.Cells[5, 2] = "75";
+            xlWorkSheet.Cells[5, 3] = "82";
+            xlWorkSheet.Cells[5, 4] = "68";
+
+            xlWorkBook.SaveAs(filePath, Excel.XlFileFormat.xlWorkbookNormal);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+
+        }
+
+    }
+}
