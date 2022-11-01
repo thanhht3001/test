ac0d37c37a6700a20f0f3543e15b80d4fca05cb1
ThanhHT <90333333+thanhht3001@users.noreply.github.com>
2022-11-01 07:02:46 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+            //gửi tên fila qua Class 1;
+            Class1 cl1 = new Class1();
+            cl1.SaveFilePath(saveFileDialog.FileName);
+
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label1.Text = e.ProgressPercentage.ToString();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                label1.Text = "Đã hủy";
+            }
+            else if (e.Error != null)
+            {
+                label1.Text = "Lỗi nè";
+            }
+            else
+            {
+                label1.Text = "Xong";
+            }
+
+
+
+
+
+        }
+    }
+}
